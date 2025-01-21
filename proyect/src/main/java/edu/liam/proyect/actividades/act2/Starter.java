8216cf3692c397adcdd5783b865f970bb1ab4102
liamsaldana <Liam.saldana13@gmail.com>
2025-01-21 11:20:42 -0600

    Añado diagrama de clases 2, starter y CLI aun incompletos commit

---
+++ proyect/src/main/java/edu/liam/proyect/actividades/act2/Starter.java
@@ -0,0 +1,6 @@
+package edu.liam.proyect.actividades.act2;
+
+public class Starter {
+    public static void main(String[] args) {
+    }
+}
---
+++ proyect/src/main/java/edu/liam/proyect/actividades/act2/ui/CLI.java
@@ -0,0 +1,5 @@
+package edu.liam.proyect.actividades.act2.ui;
+
+public class CLI {
+    
+}
